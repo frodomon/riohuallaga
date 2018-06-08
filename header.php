3ea08d6b7d607830b8bf076a2673db7b58e4ae1e
frodomon <vasquez.alf@gmail.com>
2018-06-08 17:51:15 -0500

    Header con direccion y fixed al scrollear

--- header.php
+++ header.php
@@ -4,9 +4,9 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title><?php wp_title(''); ?></title>
-  <meta name="description" content="Hotel Rio Huallaga">
-  <meta name="author" content="Ukuku">
+  <title><?php bloginfo('name'); ?> | <?php wp_title(''); ?></title>
+  <meta name="description" content="Hotel Rio Huallaga en Yurimaguas">
+  <meta name="author" content="Alfredo Vásquez">
   <!--[if lt IE 9]> <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script> <![endif]-->
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
   <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
@@ -20,18 +20,16 @@
 	<div id='wrap'>
 		<header class="group">
 			<div class='top-header'>
-				<div class="top-left hide-responsive">
-				</div>
 				<?php $url = home_url( '/' ); ?>
-				<div class="header-logo">
-					<a href="<?php echo esc_url($url) ?>" class="hide-responsive"><img id="logo-header" src="<?php print IMAGES.'/logo horizontal.png' ?>"></a>
-					<a href="<?php echo esc_url($url) ?>" class="show-responsive"><img id="logo-header" src="<?php print IMAGES.'/logo horizontal inv.png' ?>"></a>
-
+				<a href="<?php echo esc_url($url) ?>" class="hide-responsive"><img id="logo-header" src="<?php print IMAGES.'/logo horizontal.png' ?>"></a>
+				<a href="<?php echo esc_url($url) ?>" class="show-responsive"><img id="logo-header" src="<?php print IMAGES.'/logo horizontal inv.png' ?>"></a>
+				<div>
+					<p><i class="fa fa-map-marker" aria-hidden="true"></i>   Calle Arica Nro. 111, Yurimaguas, Loreto, Perú</p>
+					<p><i class="fa fa-phone" aria-hidden="true"></i><a href="tel:065353951">  (065) 353951  </a><i class="fa fa-mobile" aria-hidden="true"></i><a href="tel:+511965801755">  965801755</a></p>
+					<p><i class="fa fa-envelope" aria-hidden="true"></i><a href='mailto:recepcion@hotelriohuallaga.com.pe'>  recepcion@hotelriohuallaga.com.pe</a></p>
 				</div>
-				<div class="social">
-					<a href="https://www.facebook.com/HotelRioHuallagaOficial" target="_blank"><i class="fa fa-facebook-square fa-2x sn_icons" aria-hidden="true"></i></a>
-					<a href="https://www.tripadvisor.com/Hotel_Review-g3396866-d3824394-Reviews-Hotel_Rio_Huallaga-Yurimaguas_Loreto_Region.html" target="_blank"><i class="fa fa-tripadvisor  fa-2x sn_icons" aria-hidden="true"></i></a>
-				</div>
+				<a href="https://www.facebook.com/HotelRioHuallagaOficial" target="_blank"><i class="fa fa-facebook-square fa-2x sn_icons" aria-hidden="true"></i></a>
+				<a href="https://www.tripadvisor.com/Hotel_Review-g3396866-d3824394-Reviews-Hotel_Rio_Huallaga-Yurimaguas_Loreto_Region.html" target="_blank"><i class="fa fa-tripadvisor  fa-2x sn_icons" aria-hidden="true"></i></a>
 			</div>
 			<menu>
 				<?php wp_nav_menu( array('menu' => 'Main', 'menu_class' => 'sf-menu' )); ?>
--- js/scripts.js
+++ js/scripts.js
@@ -14,4 +14,19 @@ jQuery(document).ready(function() {
 	if ( window.location.pathname == '/' ){
 		jQuery('body').addClass('fullheight');
 	}
+
+	jQuery(window).scroll(function () {
+		if ( jQuery(window).width() < 768 ){
+			jQuery('header').addClass('fixed-header')
+			jQuery('#slideshow').css('padding-top','80px')
+		}
+		else{
+			if( jQuery(window).scrollTop() >= 121 ){
+				jQuery('menu').addClass('fixed-header');
+			}
+			else if ( (jQuery(window).scrollTop() <= 120) ){
+				jQuery('menu').removeClass('fixed-header');
+			}
+		}
+	});
 });
